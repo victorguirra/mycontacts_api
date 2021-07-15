af67bdebf1f786483eb7a719ea721563191199be
victorguirra <victor.hugo.guirra@gmail.com>
2021-07-15 14:40:11 -0300

    create contact controller

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 
-const app = express();
+const routes = require('./routes');
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello World' });
-});
+const app = express();
+app.use(routes);
 
 app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333'));
