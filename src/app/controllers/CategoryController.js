443296546fe5c7fae05d2154d96d44db1a1a6d13
victorguirra <victor.hugo.guirra@gmail.com>
2021-07-18 17:39:44 -0300

    add validations in updated method

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -40,6 +40,22 @@ class CategoryController {
     const { id } = request.params;
     const { name } = request.body;
 
+    const categoryExists = await CategoriesRepository.findById(id);
+
+    if (!categoryExists) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    if (!name) {
+      return response.status(404).json({ error: 'Name is required' });
+    }
+
+    const categoryByName = await CategoriesRepository.findByName(name);
+
+    if (categoryByName && categoryByName.id !== id) {
+      return response.status(400).json({ error: 'This name is already in use' });
+    }
+
     const category = await CategoriesRepository.update(id, name);
 
     return response.status(200).json(category);
