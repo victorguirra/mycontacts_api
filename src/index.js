cc1603f1fc008de1af773e73ca987aa7c1728184
victorguirra <victor.hugo.guirra@gmail.com>
2021-07-18 16:56:54 -0300

    handling and capturing errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -6,5 +7,10 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('❌❌❌ Error Handler ❌❌❌');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333'));
