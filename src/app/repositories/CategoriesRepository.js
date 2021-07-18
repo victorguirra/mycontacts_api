d8907e0a9f3550845ccfabd6fc17c1f5ff124466
victorguirra <victor.hugo.guirra@gmail.com>
2021-07-18 17:12:19 -0300

    create show method in controller and findById method in repository

--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -6,6 +6,11 @@ class CategoriesRepository {
     return rows;
   }
 
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM categories WHERE id = $1', [id]);
+    return row;
+  }
+
   async findByName(name) {
     const [row] = await db.query('SELECT * FROM categories WHERE name = $1', [name]);
     return row;
